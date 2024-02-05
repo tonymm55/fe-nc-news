642028d5da12c5cd85ba43f4d62d5d627365492a
TonyMM55 <tonymmoran@gmail.com>
2024-02-05 19:29:05 +0000

    add some flexbox styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,27 +4,9 @@ import { useState, useEffect } from "react";
 import Articles from "./Articles";
 
 function Home() {
-  // const [articles, setArticles] = useState();
-  // const [selectedTopic, setSelectedTopic] = useState(" ");
-
-  // useEffect(() => {
-  //   if (selectedTopic) {
-  //     fetchArticles().then((reponse) => {
-  //       setArticles(reponse.data.articles);
-  //     });
-  //   } else {
-  //     fetchArticles(selectedTopic).then((response) => {
-  //       console.log(selectedTopic, "<<< Selected Topic (Articles)");
-  //       setArticles(response.data.articles);
-  //     });
-  //   }
-  // }, [selectedTopic]);
-
   return (
     <div>
       <Articles />
-      {/* <Topic setSelectedTopic={setSelectedTopic} />
-      <ArticleList articles={articles} selectedTopic={selectedTopic} /> */}
     </div>
   );
 }
