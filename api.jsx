80f10412ccf595242b4e790f56156015b34ea8d7
TonyMM55 <tonymmoran@gmail.com>
2024-02-06 16:42:11 +0000

    remove endpoints from api.jsx

--- api.jsx
+++ api.jsx
@@ -41,14 +41,3 @@ export const fetchCommentsByArticleId = async (articleId) => {
 };
 
 export default { fetchArticles, fetchArticleById, fetchCommentsByArticleId };
-
-//Endpoints--------//
-
-// app.get("/api/topics", getAllTopics);
-// app.get("/api/articles", getAllArticles);
-// app.get("/api/articles/:article_id", getArticleById);
-// app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-// app.post("/api/articles/:article_id/comments", postCommentsByArticleId);
-// app.patch("/api/articles/:article_id", patchArticleByArticleId);
-// app.delete("/api/comments/:comment_id", deleteCommentByCommentId);
-// app.get("/api/users", getAllUsers);
